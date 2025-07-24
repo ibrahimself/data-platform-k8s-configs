310d0912663a5ebdd5a213d3bb7dc42c88ddca28
EC2 Default User <ec2-user@ip-10-0-3-148.eu-west-3.compute.internal>
2025-07-24 16:06:13 +0000

    Fix monitoring deployment

--- create-applicationset-now.sh
+++ create-applicationset-now.sh
@@ -1,11 +1,15 @@
 kubectl apply -f applications/monitoring-helm.yaml
 kubectl apply -f applications/monitoring-resources.yaml
+sleep 60
 kubectl apply -f applications/postgresql-helm.yaml
 kubectl apply -f applications/postgresql-resources.yaml
+sleep 60
 kubectl apply -f applications/spark-helm.yaml
 kubectl apply -f applications/spark-resources.yaml
+sleep 60
 kubectl apply -f applications/minio.yaml
 kubectl apply -f applications/airflow-helm.yaml
 kubectl apply -f applications/airflow-resources.yaml
+sleep 60
 kubectl apply -f applications/dremio-helm.yaml
 kubectl apply -f applications/dremio-resources.yaml
